2a940d6a109ba12ea76aad5c43ddef34e4ef0835
misango <franklinemisango@gmail.com>
2023-08-11 17:13:10 +0800

    finalized testing quantitative momentum strategizer

--- dashboard.py
+++ dashboard.py
@@ -225,7 +225,6 @@ def main():
                     covariance_graph = plt.gcf()
                     st.pyplot(covariance_graph)
                 if option == 'Level II : Equal-Weight Optimizer':
-                    st.write("Checked")
                     portfolio_size = st.number_input("Enter the value of your portfolio in ($):")
                     try:
                         val = float(portfolio_size)
@@ -259,8 +258,84 @@ def main():
                         final_dataframe.loc[i, 'Number Of Shares to Buy'] = math.floor(position_size / final_dataframe['Price'][i])          
                     st.write(final_dataframe)
 
-                if option == 'Level III : Quantitative momentum Strategizer':
-                    pass
+                if option == 'Level III: Quantitative momentum Strategizer':
+                    st.write("Checked")
+                    portfolio_size = st.number_input("Enter the value of your portfolio in ($):")
+                    try:
+                        val = float(portfolio_size)
+                    except ValueError:
+                        print("That's not a number! \n Try again:")
+                        portfolio_size = input("Enter the value of your portfolio in ($):")
+                    
+                    # Blueprints for Showcasing the Final Data Frames
+                    symbol_groups = list(chunks(st.session_state.tickers, len(st.session_state.tickers)))
+                    symbol_strings = []
+                    for i in range(0, len(symbol_groups)):
+                        symbol_strings.append(','.join(symbol_groups[i]))
+
+
+                    hqm_columns = [
+                            'Ticker', 
+                            'Price', 
+                            'Recommended Number of shares to Buy', 
+                            'One-Year Price Return', 
+                            'One-Year Return Percentile',
+                            'Six-Month Price Return',
+                            'Six-Month Return Percentile',
+                            'Three-Month Price Return',
+                            'Three-Month Return Percentile',
+                            'One-Month Price Return',
+                            'One-Month Return Percentile',
+                            'HQM Score'
+                            ]
+                    hqm_dataframe = pd.DataFrame(columns = hqm_columns)
+                    if benchmark == 'S&P 500' :
+                        for symbol_string in symbol_strings:
+                            batch_api_call_url = f'https://cloud.iexapis.com/stable/stock/market/batch/?types=stats,quote&symbols={symbol_string}&token={IEX_CLOUD_API_TOKEN}'
+                            data = requests.get(batch_api_call_url).json()
+                            for symbol in symbol_string.split(','):
+                                hqm_dataframe = hqm_dataframe.append(
+                                                                pd.Series([symbol, 
+                                                                        data[symbol]['quote']['latestPrice'],
+                                                                        'N/A',
+                                                                        data[symbol]['stats']['year1ChangePercent'],
+                                                                        'N/A',
+                                                                        data[symbol]['stats']['month6ChangePercent'],
+                                                                        'N/A',
+                                                                        data[symbol]['stats']['month3ChangePercent'],
+                                                                        'N/A',
+                                                                        data[symbol]['stats']['month1ChangePercent'],
+                                                                        'N/A',
+                                                                        'N/A'
+                                                                        ], 
+                                                                        index = hqm_columns), 
+                                                                ignore_index = True)
+                                            
+                    time_periods = [
+                            'One-Year',
+                            'Six-Month',
+                            'Three-Month',
+                            'One-Month'
+                            ]
+
+                    for row in hqm_dataframe.index:
+                        for time_period in time_periods:
+                            hqm_dataframe.loc[row, f'{time_period} Return Percentile'] = stats.percentileofscore(hqm_dataframe[f'{time_period} Price Return'], hqm_dataframe.loc[row, f'{time_period} Price Return'])/100
+
+                    # Print each percentile score to make sure it was calculated properly
+                    for time_period in time_periods:
+                        print(hqm_dataframe[f'{time_period} Return Percentile'])
+                    for row in hqm_dataframe.index:
+                        momentum_percentiles = []
+                        for time_period in time_periods:
+                            momentum_percentiles.append(hqm_dataframe.loc[row, f'{time_period} Return Percentile'])
+                        hqm_dataframe.loc[row, 'HQM Score'] = mean(momentum_percentiles)
+                    hqm_dataframe.sort_values(by = 'HQM Score', ascending = False)
+                    position_size = float(portfolio_size) / len(hqm_dataframe.index)
+                    for i in range(0, len(hqm_dataframe['Ticker'])):
+                        hqm_dataframe.loc[i, 'Recommended Number of shares to Buy'] = math.floor(position_size / hqm_dataframe['Price'][i])         
+                    st.write(hqm_dataframe)
+
                 if option == 'Level IV: Value Investing Strategizer':
                     pass
 
