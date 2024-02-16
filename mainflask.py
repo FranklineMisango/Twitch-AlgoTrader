0c8a14db70116cb305e262708c9ec6753ed110b5
misango <franklinemisango@gmail.com>
2024-02-17 00:00:28 +0800

    Comprehensive dashboard processing without ci/cd streamlit

--- mainflask.py
+++ mainflask.py
@@ -1,11 +1,14 @@
-from flask import Flask,render_template
-import configEx
+from flask import Flask, render_template
+import api
 
-app =  Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = api.convertWatchlist()
+    cleaned_data = [symbol.split(':')[1] for symbol in data]
+    print(cleaned_data)
+    return render_template('index.html', data=cleaned_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
